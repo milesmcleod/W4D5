8962f7a51e80b1cd3b3a1382da3d2aad766244f3
Miles McLeod <milesmcleod123@gmail.com>
2017-10-27 14:56:12 -0700

    Complete db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+8.times do
+  band = Band.new(name: Faker::Pokemon.name)
+  band.save
+  [2,3,4,5].sample.times do
+    album = Album.new(
+      title: Faker::Ancient.god,
+      year: (1986..2017).to_a.sample,
+      band_id: band.id,
+      studio?: [true, true, true, true, false].sample
+    )
+    album.save
+    i = 1
+
+    [5,6,9,12,18].sample.times do
+      track = Track.new(
+        title: Faker::Hipster.word,
+        ord: i,
+        lyrics: Faker::Hipster.paragraph(2),
+        bonus?: [false, false, false, false, false, false, true].sample,
+        album_id: album.id
+      )
+      track.save
+      i += 1
+    end
+  end
+end
